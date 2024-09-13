dbe430025ed97cc1a915ff95efa53068ff3907bc
Sankalp <sankalpsharma715@gmail.com>
2024-09-13 10:27:19 +0530

    Updated on 10:27 am

--- 0028_file_handling_pointer.py
+++ 0028_file_handling_pointer.py
@@ -1,8 +1,10 @@
 import sys
-new_one = open("c:\\SAMPLE\\new_one\\Answer.txt")
-line1 = new_one.readline()
-line2 = new_one.readline()
-sys.stdout.write(line1)
-sys.stdout.write(line2)
-sys.stderr.write("No error occurred\n")
+new_one = open("c:\\SAMPLE\\new_one\\Answer.txt", "w")
+# line1 = new_one.readline()
+# line2 = new_one.readline()
+# sys.stdout.write(line1)
+# sys.stdout.write(line2)
+# sys.stderr.write("No error occurred\n")
+with open("c:\\SAMPLE\\new_one\\Answer.txt","w") as new_one:
+    new_one.write("Hi there!!")
 new_one.close()
