00d074289f952b71cdce94ba9b1e7eff0c191b93
Sankalp <sankalpsharma715@gmail.com>
2024-09-13 11:11:56 +0530

    Updated on 11:11 am

--- 0028_file_handling_pointer.py
+++ 0028_file_handling_pointer.py
@@ -13,23 +13,29 @@ import pickle
 # emp2 = {"Empno":1211,"Name":"Zoya","Age":30,"Salary":48000}
 # emp3 = {"Empno":1251,"Name":"Simarjeet","Age":27,"Salary":49000}
 # emp4 = {"Empno":1266,"Name":"Alex","Age":29,"Salary":50000}
-# emp_file = open("c:\\SAMPLE\\new_one\\emp.dat","wb")
 # pickle.dump(emp1, emp_file)
 # pickle.dump(emp2, emp_file)
 # pickle.dump(emp3, emp_file)
 # pickle.dump(emp4, emp_file)
-# emp_file.close()
 
-stu = {}
-stu_file = open("c:\\SAMPLE\\new_one\\Student.dat","wb")
-ans = "y"
-while ans == "y":
-    roll_no = int(input("Enter the roll no of the student:"))
-    name = input("Enter the name of the student:")
-    marks = float(input("Enter the marks of the student:"))
-    stu["Rollno"] = roll_no
-    stu["Name"] = name
-    stu["Marks"] = marks
-    pickle.dump(stu, stu_file)
-    ans = input("Do you want to enter more records? (y/n)")
-stu_file.close()
+# stu = {}
+# stu_file = open("c:\\SAMPLE\\new_one\\Student.dat","wb")
+# ans = "y"
+# while ans == "y":
+#     roll_no = int(input("Enter the roll no of the student:"))
+#     name = input("Enter the name of the student:")
+#     marks = float(input("Enter the marks of the student:"))
+#     stu["Rollno"] = roll_no
+#     stu["Name"] = name
+#     stu["Marks"] = marks
+#     pickle.dump(stu, stu_file)
+#     ans = input("Do you want to enter more records? (y/n)")
+# stu_file.close()
+emp = {}
+emp_file = open("c:\\SAMPLE\\new_one\\emp.dat","rb")
+try:
+    while True:
+        emp = pickle.load(emp_file)
+        print(emp)
+except EOFError:
+    emp_file.close()
