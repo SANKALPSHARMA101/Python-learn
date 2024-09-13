63655386ed26b8a4826ff0965236d6c7e686e648
Sankalp <sankalpsharma715@gmail.com>
2024-09-13 11:29:29 +0530

    Updated on 11:29 am

--- 0029_Exception_Handling.py
+++ 0029_Exception_Handling.py
@@ -1,8 +1,13 @@
-ok = False
-while not ok:
-    try:
-        numberstring = input("Enter an integer:")
-        n = int(numberstring)
-        ok = True
-    except:
-        print("Error!!Not a valid integer.")
+# ok = False
+# while not ok:
+#     try:
+#         numberstring = input("Enter an integer:")
+#         n = int(numberstring)
+#         ok = True
+#     except:
+#         print("Error!!Not a valid integer.")
+try:
+    new_one = open("c:\\SAMPLE\\new_one\\myfile.txt","w")
+    print(new_one.read())
+except:
+    print("Error opening file!!")
