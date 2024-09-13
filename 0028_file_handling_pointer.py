a03c8d47f286a95aefba2ba8a1a09bf671976855
Sankalp <sankalpsharma715@gmail.com>
2024-09-13 10:44:19 +0530

    Updated on 10:44 am

--- 0028_file_handling_pointer.py
+++ 0028_file_handling_pointer.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 new_one = open("c:\\SAMPLE\\new_one\\Answer.txt", "w")
 # line1 = new_one.readline()
 # line2 = new_one.readline()
@@ -8,3 +9,13 @@ new_one = open("c:\\SAMPLE\\new_one\\Answer.txt", "w")
 with open("c:\\SAMPLE\\new_one\\Answer.txt","w") as new_one:
     new_one.write("Hi there!!")
 new_one.close()
+emp1 = {"Empno":1201,"Name":"Anushree","Age":25,"Salary":47000}
+emp2 = {"Empno":1211,"Name":"Zoya","Age":30,"Salary":48000}
+emp3 = {"Empno":1251,"Name":"Simarjeet","Age":27,"Salary":49000}
+emp4 = {"Empno":1266,"Name":"Alex","Age":29,"Salary":50000}
+emp_file = open("c:\\SAMPLE\\new_one\\emp.dat","wb")
+pickle.dump(emp1, emp_file)
+pickle.dump(emp2, emp_file)
+pickle.dump(emp3, emp_file)
+pickle.dump(emp4, emp_file)
+emp_file.close()
