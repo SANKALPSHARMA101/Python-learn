6f70f1de84b0479331514017e3f0c6e61a627bd8
Sankalp <sankalpsharma715@gmail.com>
2024-09-13 10:22:20 +0530

    Updated once 10:22 am

--- 0028_file_handling_pointer.py
+++ 0028_file_handling_pointer.py
@@ -1,3 +1,8 @@
 import sys
 new_one = open("c:\\SAMPLE\\new_one\\Answer.txt")
+line1 = new_one.readline()
+line2 = new_one.readline()
+sys.stdout.write(line1)
+sys.stdout.write(line2)
+sys.stderr.write("No error occurred\n")
 new_one.close()
