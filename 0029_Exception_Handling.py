810bb3ab2dd1d378d97e5d2f033573960a93c3d5
Sankalp <sankalpsharma715@gmail.com>
2024-09-14 11:06:03 +0530

    Updated on 11:05 am

--- 0029_Exception_Handling.py
+++ 0029_Exception_Handling.py
@@ -6,10 +6,36 @@
 #         ok = True
 #     except:
 #         print("Error!!Not a valid integer.")
+# try:
+#     new_one = open("c:\\SAMPLE\\new_one\\myfile.txt","w")
+#     print(new_one.read())
+# except:
+#     print("Error opening file!!")
+# finally:
+#     print("Finally saying goodbye!!")
+    
+# try:
+#     a = int(input("Enter the value of numerator:"))
+#     b = int(input("Enter the value of denominator:"))
+#     if b==0:
+#         raise ZeroDivisionError(str(a) + "/0 not possible")
+#     print(a/b)
+# except ZeroDivisionError as e:
+#     print("Exception",str(e))
+
 try:
-    new_one = open("c:\\SAMPLE\\new_one\\myfile.txt","w")
-    print(new_one.read())
+    new_one = open("c:\\SAMPLE\\new_one\\myfile.txt")
+    my_line = new_one.readline()
+    my_int = int(new_one.strip())
+    my_calc_value = 101/my_int
+except IOError:
+    print("I/O error occurred")
+except ValueError:
+    print("Value error occurred")
+except ZeroDivisionError:
+    print("Division by zero error occurred")
 except:
-    print("Error opening file!!")
-finally:
-    print("Finally saying goodbye!!")
+    print("Unexcepted error")
+else:
+    print("Let's go!! No exceptions>")
+    
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,7 @@
-in range(2):
-#     print("Enter the details for the students",(i+1),"below:")
-#     rollno = int(input("Roll no:"))
-#     name = input("Name:")
-#     marks = float(input("Marks:"))
-#     rec = str(rollno) +","+ name +","+ str(marks)+'\n'
-#     new_one.write(rec) 
+
+#     new_one = open("c:\\SAMPLE\\new_one\\myfile.txt","w")
+#     print(new_one.read())
+# except:
+#     print("Error opening file!!")
+# finally:
+#     print("Finally saying goodbye!!")
